641cb2c9ee2c458bac356dda858d813202b0280b
wiktoriamusialska <wiktoria.musialska@interia.pl>
2024-10-27 09:51:54 +0100

    Set check in and check out

--- src/test/java/HotelSearch.java
+++ src/test/java/HotelSearch.java
@@ -15,6 +15,8 @@ public class HotelSearch {
         driver.findElement(By.xpath("//span[text()='Search by Hotel or City Name']")).click();
         driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("Dubai");
         driver.findElement(By.xpath("//span[@class ='select2-match' and text() = 'Dubai']")).click();
+        driver.findElement(By.name("checkin")).sendKeys("17/04/2021");
+        driver.findElement(By.name("checkout")).sendKeys("20/04/2021");
         driver.quit();
     }
 }
