10501b5966c21bde9c659632b31524947f443f72
wiktoriamusialska <wiktoria.musialska@interia.pl>
2024-10-27 09:31:14 +0100

    Set city name

---
+++ src/test/java/HotelSearch.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class HotelSearch {
+    @Test
+    public void searchHotel() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get("http://www.kurs-selenium.pl/demo/");
+        driver.findElement(By.xpath("//span[text()='Search by Hotel or City Name']")).click();
+        driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("Dubai");
+        driver.findElement(By.xpath("//span[@class ='select2-match' and text() = 'Dubai']")).click();
+        driver.quit();
+    }
+}
