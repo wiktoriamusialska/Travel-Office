e8c1199d2a1d0a593045e7cbf653709a5e55bfc6
wiktoriamusialska <wiktoria.musialska@interia.pl>
2024-10-27 10:30:11 +0100

    Full path for searching of hotel

--- src/test/java/HotelSearch.java
+++ src/test/java/HotelSearch.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class HotelSearch {
     @Test
@@ -16,7 +18,22 @@ public class HotelSearch {
         driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("Dubai");
         driver.findElement(By.xpath("//span[@class ='select2-match' and text() = 'Dubai']")).click();
         driver.findElement(By.name("checkin")).sendKeys("17/04/2021");
-        driver.findElement(By.name("checkout")).sendKeys("20/04/2021");
+        driver.findElement(By.name("checkout")).click();
+        driver.findElements(By.xpath("//td[@class='day ' and text()='30']"))
+                .stream()
+                .filter(el -> el.isDisplayed())
+                .findFirst()
+                .ifPresent(el ->el.click());
+        driver.findElement(By.id("travellersInput")).click();
+        driver.findElement(By.id("adultPlusBtn")).click();
+        driver.findElement(By.id("childPlusBtn")).click();
+        driver.findElement(By.xpath("//button[text()=' Search']")).click();
+        List<String> hotelNames = driver.findElements(By.xpath("//h4[contains(@class,'list_title')]//b"))
+                .stream()
+                .map(el -> el.getText())
+                .collect(Collectors.toList());
+        System.out.println("Names of the hotels: " + hotelNames);
+        System.out.println("Number of the hotels "+ hotelNames.size());
         driver.quit();
     }
 }
