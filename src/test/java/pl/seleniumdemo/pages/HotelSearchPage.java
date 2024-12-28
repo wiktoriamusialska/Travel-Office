c151da62b18fc22484da23837081b76184516593
wiktoriamusialska <wiktoria.musialska@interia.pl>
2024-12-28 11:51:41 +0100

    Add logs

--- src/test/java/pl/seleniumdemo/pages/HotelSearchPage.java
+++ src/test/java/pl/seleniumdemo/pages/HotelSearchPage.java
@@ -1,5 +1,7 @@
 package pl.seleniumdemo.pages;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -7,6 +9,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import java.util.List;
+
 
 
 public class HotelSearchPage {
@@ -42,27 +45,36 @@ public class HotelSearchPage {
 
     private WebDriver driver;
 
+    private static final Logger logger = LogManager.getLogger();
+
     public HotelSearchPage(WebDriver driver) {
         PageFactory.initElements(driver, this);
         this.driver = driver;
     }
 
+
     public void setCity(String cityName) {
+        logger.info("method setCity - start");
         searchHotelSpan.click();
         searchHotelInput.sendKeys(cityName);
         String xpath = String.format("//span[@class='select2-match' and text()='%s']", cityName);
         driver.findElement(By.xpath(xpath)).click();
+        logger.info("method setCity - end");
     }
 
     public void setDates(String checkin, String checkout) {
+        logger.info("method setDates - start");
         checkinInput.sendKeys(checkin);
         checkoutInput.sendKeys(checkout);
+        logger.info("method setDates - end");
     }
 
     public void setTravellers(int adultsToAdd, int childToAdd) {
+        logger.info("method setTravellers - start");
         travellerInput.click();
         addTraveler(adultPlusBtn, adultsToAdd);
         addTraveler(childPlusBtn, childToAdd);
+        logger.info("method setTravellers - end");
     }
 
     private void addTraveler(WebElement travelerBtn, int numberOfTravelers) {
@@ -72,13 +84,18 @@ public class HotelSearchPage {
     }
 
     public void performSearch() {
+
+        logger.info("method performSearch - start");
         searchButton.click();
+        logger.info("method performSearch - end");
     }
 
 
     public void openSignUpForm() {
+        logger.info("method openSignUpForm - start");
         myAccountLink.stream().filter(WebElement::isDisplayed).findFirst().ifPresent(WebElement::click);
         signUpLink.get(1).click();
+        logger.info("method openSignUpForm - end");
     }
 
 
